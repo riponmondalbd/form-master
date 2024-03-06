36bcf6df32a5668b61b8c822481607c2030dddf7
Ripon Mondal <riponmondal149@gmail.com>
2024-03-06 23:36:19 +0600

    form with state to get  value  and set default  value

--- src/__form__.js
+++ src/__form__.js
@@ -1,4 +1,6 @@
 /**
- * 1. use form tag and name to gate values;
+ * 1. use form tag and onSubmit with event (e) from  e access e.target.[name of the input field].value 
+ * 
+ * 2. use individual field  state
  * 
  */
