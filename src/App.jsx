5b13d6fcdc36afa8571b32f6bd9bada741867e1f
Ripon Mondal <riponmondal149@gmail.com>
2024-03-07 19:19:52 +0600

    custom hook and create my first custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import HookForm from './components/HookForm/HookForm'
 import RefForm from './components/RefForm/RefForm'
 import StatefulForm from './components/StatefulForm/StatefulForm'
 // import SimpleForm from './components/SimpleForm/SimpleForm'
@@ -10,7 +11,8 @@ function App() {
       <h1>Vite + React</h1>
       {/* <SimpleForm></SimpleForm> */}
       {/* <StatefulForm></StatefulForm> */}
-      <RefForm></RefForm>
+      {/* <RefForm></RefForm> */}
+      <HookForm></HookForm>
     </>
   )
 }
