8aaca8b58cebfc09aac45d2530eb84917009ead4
Ripon Mondal <riponmondal149@gmail.com>
2024-03-13 18:48:35 +0600

    how to use context api to avoid prop drilling

--- src/components/Grandpa/Grandpa.jsx
+++ src/components/Grandpa/Grandpa.jsx
@@ -1,23 +1,30 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import Aunty from "../Aunty/Aunty";
 import Dad from "../Dad/Dad";
 import Uncle from "../Uncle/Uncle";
 import './Grandpa.css';
 
-const AssetContext = createContext('gold');
+// make and export context API
+export const AssetContext = createContext('gold');
+export const MoneyContext = createContext(1000);
 
 const Grandpa = () => {
+    const [money, setMoney] = useState(1000);
     const asset = 'diamond';
+
     return (
         <div className="grandpa">
             <h2>Grandpa</h2>
-            <AssetContext.Provider value="gold">
-                <section className="flex">
-                    <Dad asset={asset}></Dad>
-                    <Uncle asset={asset}></Uncle>
-                    <Aunty></Aunty>
-                </section>
-            </AssetContext.Provider>
+            <p>Net Money: {money}</p>
+            <MoneyContext.Provider value={[money, setMoney]}>
+                <AssetContext.Provider value="gold">
+                    <section className="flex">
+                        <Dad asset={asset}></Dad>
+                        <Uncle asset={asset}></Uncle>
+                        <Aunty></Aunty>
+                    </section>
+                </AssetContext.Provider>
+            </MoneyContext.Provider>
         </div>
     );
 };
@@ -25,7 +32,7 @@ const Grandpa = () => {
 export default Grandpa;
 
 /**
- * 1. create a context and export
- * 2. add provider for the context with a value
- * 
+ * 1. create a context and export it
+ * 2. add provider for the context with a value. value could be anything
+ * 3. useContext to access value in the context API.
  */
