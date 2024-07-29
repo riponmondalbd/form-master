7d8f06a32bf530bb3c41d11294d9b9665b1352c1
riponmondalbd <riponmondal149@gmail.com>
2024-07-29 21:29:49 +0600

    clear all previous code

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import Grandpa from './components/Grandpa/Grandpa'
-// import ReusableForm from './components/ReusableForm/ReusableForm'
-// import HookForm from './components/HookForm/HookForm'
-// import RefForm from './components/RefForm/RefForm'
-// import StatefulForm from './components/StatefulForm/StatefulForm'
-// import SimpleForm from './components/SimpleForm/SimpleForm'
 
 function App() {
-
-  // const handleSignUpSubmit = data => {
-  //   console.log('sign up data', data);
-  // }
-
-  // const handleUpdateProfile = data => {
-  //   console.log('Update profile', data);
-  // }
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Form Master</h1>
-      <Grandpa></Grandpa>
-      {/* <SimpleForm></SimpleForm> */}
-      {/* <StatefulForm></StatefulForm> */}
-      {/* <RefForm></RefForm> */}
-      {/* <HookForm></HookForm> */}
-      {/* <ReusableForm
-        formTitle={'Sign Up'}
-        handleSubmit={handleSignUpSubmit}>
-        <div>
-          <h2>Sign Up</h2>
-          <p>Please sign up right now</p>
-        </div>
-      </ReusableForm>
-      <ReusableForm
-        formTitle={'Profile Update'}
-        handleSubmit={handleUpdateProfile}
-        submitBtnText='Update'>
-        <div>
-          <h2>Update Profile</h2>
-          <p>Always keep your profile updated</p>
-        </div>
-      </ReusableForm> */}
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
