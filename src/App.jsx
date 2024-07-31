baa1db560772969650daa8ce4deae2cfc9b93645
riponmondalbd <riponmondal149@gmail.com>
2024-08-01 01:52:01 +0600

    explore ref, controled and uncontrolled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import StatefulForm from "./components/StatefulForm/StatefulForm";
+import RefForm from "./components/RefForm/RefForm";
+// import StatefulForm from "./components/StatefulForm/StatefulForm";
 // import SimpleForm from "./components/SimpleForm/SimpleForm";
 
 function App() {
@@ -7,7 +8,8 @@ function App() {
     <>
       <h1>Vite + React</h1>
       {/* <SimpleForm /> */}
-      <StatefulForm />
+      {/* <StatefulForm /> */}
+      <RefForm />
     </>
   );
 }
